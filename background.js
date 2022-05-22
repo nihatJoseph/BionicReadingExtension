92bd1f326c3fa63225c218a77692ffb34cd1810b
Nihat Yusuf <nihatyusufuslu@gmail.com>
2022-05-22 10:07:32 +0300

    starting

---
+++ background.js
@@ -0,0 +1,15 @@
+console.log("background");
+chrome.runtime.onInstalled.addListener(({ reason, version }) => {
+  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    showReadme();
+  }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  showReadme();
+});
+
+function showReadme(info, tab) {
+  let url = chrome.runtime.getURL("readme.html");
+  chrome.tabs.create({ url });
+}
